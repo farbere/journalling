7e95ef8f4659893721c3d15c92d9234fa3d42a4b
Ethan <89481346+farbere@users.noreply.github.com>
2024-06-09 20:13:53 -0400

    more work on utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,39 @@
+import datetime
 class Activity:
-    def __init__(self):
+    # an activity is an instance of engaging with a topic
+    # each activity should have the following information:
+    # date
+    # duration (minutes)
+    # description
+    # def __init__(self, date=None, mins_dur=None, description=None):
+    #     self.date = datetime.date.today() if date is None else date
+    #     self.mins_dur = 0 if mins_dur is None else mins_dur
+    #     self.description = '' if description is None else description
+    
+    def __init__(
+            self,
+            date: datetime.date = datetime.date.today(),
+            mins_dur: int = 0,
+            description: str = ''
+        ):
+        self.date = date
+        self.mins_dur = mins_dur
+        self.description = description
+
+    def change_date(self, date: datetime.date) -> None:
+        self.date = date
+    
+    def change_duration(self, dur: int) -> None:
+        self.mins_dur = dur
+
+    def change_description(self, description: str) -> None:
+        self.description = description
+    
+    def __repr__(self) -> str:
+        return f'Activity({self.date}, {self.mins_dur}, {self.description[:20]}...)'
 
 class Topic:
-    def __init__(self, name, type, interval = 'Daily'):
+    def __init__(self, name: str, type: str, interval: str = 'Daily'):
         self.name = name
 
         # hobby, exercise, chore, etc
@@ -28,5 +59,26 @@ class Topic:
         # list of Activities
         self.activities = []
     
+    def __repr__(self) -> str:
+        return f'Topic({self.name}, {self.type}, {self.interval})'
+    
     # add activity
-    def add_activity(self,):
+    def add_activity(
+            self,
+            date: datetime.date = datetime.date.today(),
+            duration: int = 0
+        ):
+
+        # description instantiated as empty string
+        act = Activity(date, duration)
+
+        description = input('Describe the activity.')
+        act.change_description(description)
+
+        self.activities += [act]
+
+
+        
+        
+
+
